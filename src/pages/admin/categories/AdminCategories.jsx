61ea14d37202a5e98181d05657c86694dba6e556
Iqra Khaliq <114660239+Iqra562@users.noreply.github.com>
2023-10-31 00:00:39 -0700

    showAllCategories

--- src/pages/admin/categories/AdminCategories.jsx
+++ src/pages/admin/categories/AdminCategories.jsx
@@ -1,8 +1,56 @@
-import React from "react";
-function AdminCategories(){
+import {React,useMemo} from "react";
+import { useQuery } from "react-query";
+import { Button,Col,
+Modal,Row,Table,message } from "antd";
+import {  useNavigate } from "react-router-dom";
+import { CategoriesServices } from "../../../services/categories.services";
+import {AuthenticatedRoutesNames} from "../../../utilities/util.constant";
+  function AdminCategories(){
+    const navigate = useNavigate();
+    const {data: categoryData,isLoading:categoryLoading} = useQuery("categories",()=>CategoriesServices.getCategories());
+const catData = useMemo(
+    ()=>categoryData?.data?.results,[categoryData?.data?.results]
+)
+const columns = [
+  {
+    title:"Id",
+    key:"id",
+    render:(singleData)=>{
+      return singleData.cat_id; 
+    },
+  }
+]
+
     return(
         <div>
-            <h3>Categories</h3>
+           {/* {contextHolder} */}
+           <Row    type="flex"
+        justify="space-between"
+        align="middle"
+        style={{ marginBottom: "20px" }}>
+
+<Col>
+          <h3
+            style={{
+              marginBottom: "0",
+              marginTop: "0",
+            }}
+          >
+            Categories
+          </h3>
+        </Col>
+        <Col>
+          <Button
+            type="primary"
+            onClick={() => {
+                navigate(AuthenticatedRoutesNames.CATEGORY_ADD);
+            }}
+          >
+            + Add Category
+          </Button>
+        </Col>
+           </Row>
+           <Table dataSource={catData} columns={columns}/>
         </div>
     )
 }
