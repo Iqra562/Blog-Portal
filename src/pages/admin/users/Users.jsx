9dcebbd433cd915f6f3be24bfc4d2f25ffdb53a3
Iqra Khaliq <iqra44938@gmail.com>
2023-11-21 21:59:46 -0800

    deleteUserFunction

--- src/pages/admin/users/Users.jsx
+++ src/pages/admin/users/Users.jsx
@@ -1,18 +1,41 @@
 import {React,useMemo} from "react";
-import { useQuery } from "react-query";
+import { useMutation, useQuery } from "react-query";
 import GridView from "../../../components/GridView/GridView";
 import { UserServices } from "../../../services/user.service";
 import { AuthenticatedRoutesNames } from "../../../utilities/util.constant";
 import { useNavigate } from "react-router-dom";
 import { UtilService } from "../../../utilities/util.service";
+import { Button, message,Modal } from "antd";
+import { ExclamationCircleOutlined } from "@ant-design/icons";
+const {confirm} =Modal;
 
 function Users(){
     const navigate= useNavigate();
+    const [messageApi,contextHolder]= message.useMessage();
     // get users data through useQuery 
-    const {data:usersData,isLoading:usersDataLoader}=useQuery("usersData",UserServices.getUsers);
+    const {data:usersData,isLoading:usersDataLoader, refetch:refetchUserData}=useQuery("usersData",UserServices.getUsers);
     // userdata memoization 
     const usersDataMemoization = useMemo(()=> usersData?.data?.results,[usersData?.data?.results]);
-  //  console.log(usersDataMemoization)
+    //  console.log(usersDataMemoization)
+    //   delete user query 
+  const {mutateAsync:deleteUserId, isLoading:deleteUserLoader}=useMutation(UserServices.deleteUserById);
+  const deleteUserFunction=(userId)=>{
+    confirm({
+        title: "Do you want to delete post?",
+        icon:<ExclamationCircleOutlined/>,
+        onOk(){
+            deleteUserId(userId,{
+                onSuccess:()=>{
+                    messageApi.success("User is deleted successfully!");
+                refetchUserData();
+                }
+
+            })
+        },
+        onCancel(){}
+    })
+
+  }
     const columns = [{
         title:"User Id",
         render:(singleData)=>{
@@ -67,10 +90,24 @@ return singleData.is_online
         return UtilService.convertDateToMyFormat(singleData.updated_at)
 
     }
-}]
+},{
+    title:"Edit",
+    render:()=>{
+    return    <Button type="primary" >Edit</Button>
+
+    }
+ },
+ {
+    title:"Delete",
+    render:(singleData)=>{
+return <Button type="Default" onClick={()=>deleteUserFunction(singleData.user_id)}>Delete</Button>
+    }
+}
+]
 return(
-<>
+<div>
+{contextHolder}
 <GridView loading = {usersDataLoader} dataSource={usersDataMemoization} columns={columns} headings="USers"  addBtnClick={()=>navigate(AuthenticatedRoutesNames.CREATE_POST)} addBtnText="+ Add Post"/>
-</>
+</div>
 )}
 export default Users;
